1bcb210ee6761a1c75b0a2be8561fa2fb677411d
lang94 <lang94@team.com>
2024-01-24 20:24:24 +0800

    算法 learn

--- magic/aes/aes_xhs_6611/xhs.cpp
+++ magic/aes/aes_xhs_6611/xhs.cpp
@@ -36,6 +36,12 @@ void print2(char *in, int num){
     printf("\n");
 }
 
+
+void print2(char *in, int num);
+extern "C"{
+char* decrypt_hmac2(const char *AESKEY, const char *AESIV, const char* encry);
+}
+
 char* decrypt_hmac2(const char *AESKEY, const char *AESIV, const char* encry){
 
         // aes key
